9bc1be101bf93642ebce23795a72bf8fd69e1dcf
jvlaar <github@josvlaar.nl>
2018-04-13 09:56:28 +0200

    Fixed state saving

--- app/src/main/java/com/example/jos/firstapp/MainActivity.java
+++ app/src/main/java/com/example/jos/firstapp/MainActivity.java
@@ -18,14 +18,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null) {
+            return;
+        }
         names = Arrays.asList("Body", "Arms", "Ears", "Eyebrows", "Glasses", "Eyes",
                 "Hat", "Mouth", "Moustache", "Nose", "Shoes");
         //TODO: Iterate over names and restore saved state
+        for (String name: names) {
+            Boolean state = (savedInstanceState.getInt(name) != 0);
+            CheckBox checkbox = findViewById(MainActivity.this.getResources()
+                    .getIdentifier("checkBox" + name, "id", getBaseContext().getPackageName()));
+            ImageView image = (ImageView) findViewById(MainActivity.this.getResources()
+                    .getIdentifier("imageView" + name, "id", getBaseContext().getPackageName()));
+
+            if (state) {
+                image.setVisibility(View.VISIBLE);
+            } else {
+                image.setVisibility(View.INVISIBLE);
+            }
+            checkbox.setChecked(state);
+        }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        names = Arrays.asList("Body", "Arms", "Ears", "Eyebrows", "Glasses", "Eyes",
+                "Hat", "Mouth", "Moustache", "Nose", "Shoes");
+        for (String name: names) {
+            CheckBox checkbox = findViewById(MainActivity.this.getResources()
+                    .getIdentifier("checkBox" + name, "id", getBaseContext().getPackageName()));
+            outState.putInt(name, (checkbox.isChecked()) ? 1 : 0);
+        }
     }
 
     public void checkClicked(View v) {
-        Log.d("Potatoe", "checkClicked");
-
         CheckBox checkBox = (CheckBox) v;
         String checkBoxName = checkBox.getText().toString();
 
@@ -37,10 +64,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             image.setVisibility(View.INVISIBLE);
         }
-    }
-
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //TODO: Iterate over names and save state
     }
 }
